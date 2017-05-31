0ca60ee990671abf80670b5e859533e4b0adf425
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-31 17:30:00 +0200

    Trip Advisor scraper operationnel for all pages of Lyon's restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,11 @@ happy_seeds = TripAdvisor.new.scrap
 
 puts "Creating seeds"
 
+
+i = 0
+
 hashy_seeds.each do |elem|
   Restaurant.create!(name: elem[:name], address: elem[:address], ranking: elem[:ranking])
-  puts "Seed ok"
+  i += 1
+  puts "Seed #{i} ok"
 end
