9cc2291cf6381b8e7967bd487d05ca44c0236892
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-31 13:57:16 +0200

    trip scrapper udpate to cook and value rank for all

--- app/scrappers/trip_advisor.rb
+++ app/scrappers/trip_advisor.rb
@@ -5,7 +5,7 @@ class TripAdvisor
   end
 
   def scrap
-    TripAdvisor.scrap_show_one
+    TripAdvisor.scrap_show
   end
 
   def self.scrap_index
@@ -19,11 +19,9 @@ class TripAdvisor
     @resto_url = []
 
     # Récupérer tableau de liens de restaurants
-    # scrapping.search('.property_title').each do |resto|
-    #   @resto_url << resto.attribute('href')
-    # end
-
-    @resto_url << scrapping.search('.property_title').first.attribute('href')
+    scrapping.search('.property_title').each do |resto|
+      @resto_url << resto.attribute('href')
+    end
 
     # Tableau des liens sans le préfixe https://www.tripadvisor.fr/
     @resto_url = @resto_url.map { |rest| rest.value }
@@ -41,28 +39,12 @@ class TripAdvisor
         name: scrapping.search('#HEADING').text.strip,
         address: scrapping.search('.street-address').first.text,
         ranking: scrapping.search('.ui_bubble_rating.bubble_45').first.attribute('content').text.gsub(/,/, '.').to_f,
-        cook_rank: scrapping.search('.barChart.ui_bubble_rating.bubble_45').first.attribute('alt'),
-        value_balance: scrapping.search('.barChart.ui_bubble_rating.bubble_45').first.attribute('alt')
+        cook_rank: scrapping.search('.barChart .ui_bubble_rating').first.attribute('alt').value.split(' ').first.to_f,
+        value_balance: scrapping.search('.barChart .ui_bubble_rating').first.attribute('alt').value.split(' ').first.to_f
       }
     end
     results
   end
-
-  def self.scrap_show_one
-    results = []
-    resto = @resto_url.first
-    complete_url = RestClient.get ("https://www.tripadvisor.fr" + resto)
-    scrapping = Nokogiri::HTML.parse(complete_url)
-    binding.pry
-    results << {
-      name: scrapping.search('#HEADING').text.strip,
-      address: scrapping.search('.street-address').first.text,
-      ranking: scrapping.search('.ui_bubble_rating.bubble_45').first.attribute('content').text.gsub(/,/, '.').to_f,
-      cook_rank: scrapping.search('.barChart .ui_bubble_rating').first.attribute('alt').value.split(' ').first.to_f,
-      value_balance: scrapping.search('.barChart .ui_bubble_rating').first.attribute('alt').value.split(' ').first.to_f
-    }
-  end
-
 end
 
 
