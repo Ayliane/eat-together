716a3c9fca0578687242153015bdcaa3c05cb16b
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-31 18:42:57 +0200

    It works for one person :)

--- app/assets/javascripts/restaurants_index.js
+++ app/assets/javascripts/restaurants_index.js
@@ -1,25 +1,21 @@
-$(function() {
-  if ($('#restaurants-list').length > 0) {
-    $.ajax({
+$(document).ready(function() {
+  console.log('executed');
+   if ($('#restaurants-list').length > 0) {
+     var foodType1 = $('#food_type_1').val();
+     var foodType2 = $('#food_type_2').val();
+     $.ajax({
+       type: 'GET',
+       url: '/foodora?food_type=' + foodType1,
+       success: function(response) {
+         $('#restaurants-list').append(response);
+       }
+     })
+     $.ajax({
       type: 'GET',
-      url: '/foodora,
+      url: '/deliveroo?food_type=' + foodType1,
       success: function(response) {
         $('#restaurants-list').append(response);
       }
-    })
-  }
-});
-
-
-// $(function() {
-//   if ($('#restaurants-list').length > 0) {
-//     var foodType $('#food_type').val();
-//     $.ajax({
-//       type: 'GET',
-//       url: '/foodora?food_type=' + foodType,
-//       success: function(response) {
-//         $('#restaurants-list').append(response);
-//       }
-//     })
-//   }
-// });
+     })
+   }
+ });
