d44f4f19a7fd6b9dc0b423d0d9a13e2c1db0be67
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-06-01 18:27:14 +0200

    javascripting the request

--- app/assets/javascripts/restaurants_index.js
+++ app/assets/javascripts/restaurants_index.js
@@ -1,35 +1,61 @@
 $(document).ready(function() {
-  console.log('executed');
-   if ($('#restaurants-list').length > 0) {
-     var foodType1 = $('#food_type_1').val();
-     var foodType2 = $('#food_type_2').val();
-     $.ajax({
+  if ($('#restaurants-list').length > 0) {
+      var foodType1 = $('#food_type_1').val();
+      var foodType2 = $('#food_type_2').val();
+      foodTypeRequest1(foodType1);
+      foodTypeRequest2(foodType2);
+      $('#selection_food_type_1').on('change', function() {
+        var foodType = $('#selection_food_type_1').val();
+        console.log('executed');
+        $("#restaurants-list").empty();
+        console.log('executed too');
+        foodTypeRequest1(foodType);
+      });
+      $('#selection_food_type_2').on('change', function() {
+        var foodType = $('#selection_food_type_2').val();
+        $("#restaurants-list-2").empty();
+        foodTypeRequest2(foodType);
+      });
+   }
+ });
+
+function foodTypeRequest1(typeOfFood) {
+    $.ajax({
        type: 'GET',
-       url: '/foodora?food_type=' + foodType1,
+       url: '/foodora?food_type=' + typeOfFood,
        success: function(response) {
          $('#restaurants-list').append(response);
        }
      })
      $.ajax({
       type: 'GET',
-      url: '/deliveroo?food_type=' + foodType1,
+      url: '/deliveroo?food_type=' + typeOfFood,
       success: function(response) {
         $('#restaurants-list').append(response);
       }
      })
+}
+
+function foodTypeRequest2(typeOfFood) {
      $.ajax({
        type: 'GET',
-       url: '/foodora?food_type=' + foodType2,
+       url: '/foodora?food_type=' + typeOfFood,
        success: function(response) {
          $('#restaurants-list-2').append(response);
        }
      })
       $.ajax({
         type: 'GET',
-        url: '/deliveroo?food_type=' + foodType2,
+        url: '/deliveroo?food_type=' + typeOfFood,
         success: function(response) {
           $('#restaurants-list-2').append(response);
       }
      })
-   }
- });
+}
+
+
+
+
+
+
+
