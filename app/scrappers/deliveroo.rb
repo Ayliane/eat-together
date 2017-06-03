ce7e6dae713df14ab6f1b74620f1eafa7012e826
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-06-03 17:38:15 +0200

    deliveroo show scraper wip

--- app/scrappers/deliveroo.rb
+++ app/scrappers/deliveroo.rb
@@ -40,11 +40,52 @@ class Deliveroo
     end
 
     def get_scrap_from_show(url)
+      # 19 place Tolozan, Lyon => Guy and Sons Tupin
+      # https://deliveroo.fr/fr/menu/lyon/hotel-de-ville/wazza-pizza?day=today&rpos=0&time=ASAP
+      # a => href index soit l'url :
+      # response_json[:url] = /menu/lyon/hotel-de-ville/wazza-pizza?day=today&rpos=0&time=ASAP
 
+      # 2 Place Bellecour, Lyon => Guy and Sons Tupin
+      # https://www.foodora.fr/restaurant/s2of/guyandsonstupin
+
+      # Sélectionne la page à scrapper
+      @deliveroo_restaurant_url = RestClient.get ('https://deliveroo.fr/fr/') + url
+      Nokogiri::HTML.parse(@deliveroo_restaurant_url)
     end
 
     def scrap_show(url)
 
+      scrap = get_scrap_from_show(url)
 
+      @deliveroo_restaurant_menu = []
+
+      # Category title and description into an aray of hashes :
+      scraped_titles = scrap.search('.results-group .results-group-title').map(&:text).map(&:strip)
+      # stock un aray de titres sur lequel on itère ensuite... :
+
+      scraped_titles.each_with_index do |title, index|
+        # dish_lists = scrap.search('.results-list').first
+        # dish_lists = scrap.search('.results-list')[0].search('li').text
+
+        dish_lists = scrap.search('.results-list').each_with_index do |div, index|
+
+        # dish_lists.each_with_index do |div, index|
+          # dish_list = scrap.search('.menu__items .dish-list')[1].search('li').first
+          dish_card = div.search('div.dish-card').first
+          dish_hash = JSON.parse(dish_card.attribute('data-object'))
+
+          @foodora_restaurant_menu << {
+            category_title: title,
+            data: {
+              name: dish_hash['name'],
+              description: dish_hash['description'],
+              price: dish_hash['product_variations'].first['price']
+            }
+          }
+
+        end
+      end
+      @deliveroo_restaurant_menu
     end
+  end
 end
