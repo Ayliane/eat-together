f887c1c34a412da51612c59342b060f85d293e7b
kevcha <kevin.chavanne@gmail.com>
2017-06-05 21:54:34 +0200

    Refactor RestaurantRemote model

--- app/models/restaurant_remote.rb
+++ app/models/restaurant_remote.rb
@@ -1,9 +1,34 @@
 class RestaurantRemote
+  CATEGORIES = {
+    french: "Français",
+    burgers: "Burgers",
+    american: "Américain",
+    thai: "Thai",
+    international: "International",
+    vegan: "Végétarien",
+    bagels: "Bagels",
+    asian: "Asiatique",
+    italian: "Italien",
+    indian: "Indien",
+    tacos: "Tacos",
+    vietnamese: "Vietnamien",
+    sushi: "Sushi",
+    pizza: "Pizza"
+  }
 
-  CATEGORIES = {french: "Français", burgers: "Burgers", american: "Américain", thai: "Thai", international: "International", vegan: "Végétarien", bagels: "Bagels", asian: "Asiatique", italian: "Italien", indian: "Indien", tacos: "Tacos",
-                vietnamese: "Vietnamien", sushi: "Sushi", pizza: "Pizza"}
-
-  attr_accessor :name, :delivery_time, :address, :photo_url, :price_fork, :food_type, :food_characteristics, :url, :global_rank, :cook_rank, :quality_rank
+  attr_accessor(
+    :name,
+    :delivery_time,
+    :address,
+    :photo_url,
+    :price_fork,
+    :food_type,
+    :food_characteristics,
+    :url,
+    :global_rank,
+    :cook_rank,
+    :quality_rank
+  )
 
   def initialize(args)
     @name = args[:name]
@@ -14,39 +39,25 @@ class RestaurantRemote
     @food_characteristics = args[:food_characteristics]
     @delivery_time = args[:delivery_time]
     @url = args[:url]
+    @restaurant = find_restaurant
     @global_rank = global_ranking
     @cook_rank = cook_ranking
     @quality_rank = quality_ranking
   end
 
+  private
+
   def global_ranking
-    if find_restaurant == nil
-      "N/A"
-    else
-      @restaurant = find_restaurant
-      @restaurant.ranking
-    end
+    @restaurant.ranking if @restaurant.present?
   end
 
   def cook_ranking
-    if find_restaurant == nil
-      "N/A"
-    else
-      @restaurant = find_restaurant
-      @restaurant.cook_rank
-    end
+    @restaurant.cook_rank if @restaurant.present?
   end
 
   def quality_ranking
-    if find_restaurant == nil
-      "N/A"
-    else
-      @restaurant = find_restaurant
-      @restaurant.value_balance
-    end
+    @restaurant.value_balance if @restaurant.present?
   end
-
-  private
 
   def find_restaurant
     Restaurant.find_by('name iLIKE ? OR address iLike ?', "%#{@name}%", "%#{address}%")
