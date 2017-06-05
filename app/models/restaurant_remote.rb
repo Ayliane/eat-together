a5e85c94f42e30cdae1441fc423626115fbfa3be
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-06-05 13:04:36 +0200

    Correcting seeds

--- app/models/restaurant_remote.rb
+++ app/models/restaurant_remote.rb
@@ -3,7 +3,7 @@ class RestaurantRemote
   CATEGORIES = {french: "Français", burgers: "Burgers", american: "Américain", thai: "Thai", international: "International", vegan: "Végétarien", bagels: "Bagels", asian: "Asiatique", italian: "Italien", indian: "Indien", tacos: "Tacos",
                 vietnamese: "Vietnamien", sushi: "Sushi", pizza: "Pizza"}
 
-  attr_accessor :name, :delivery_time, :address, :photo_url, :price_fork, :food_type, :food_characteristics, :url
+  attr_accessor :name, :delivery_time, :address, :photo_url, :price_fork, :food_type, :food_characteristics, :url, :global_rank, :cook_rank, :quality_rank
 
   def initialize(args)
     @name = args[:name]
@@ -14,5 +14,24 @@ class RestaurantRemote
     @food_characteristics = args[:food_characteristics]
     @delivery_time = args[:delivery_time]
     @url = args[:url]
+    @global_rank = global_ranking
+    @cook_rank = cook_ranking
+    @quality_rank = quality_ranking
+  end
+
+  def global_ranking
+    if Restaurant.find_by(address: @address) == nil
+      return "N/A"
+    else
+      @restaurant = Restaurant.find_by(address: @adress)
+      @restaurant.ranking
+    end
+  end
+
+  def cook_ranking
+  end
+
+  def quality_ranking
+
   end
 end
