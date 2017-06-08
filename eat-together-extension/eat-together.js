32504a8b7b5a3d83e9cb41be8e5e4d187748a131
kevcha <kevin.chavanne@gmail.com>
2017-06-08 11:28:43 +0200

    Refactor plugin javascripts

--- eat-together-extension/eat-together.js
+++ eat-together-extension/eat-together.js
@@ -1,5 +1,26 @@
-$(function() {
+function enhanceFoodoraUI() {
+  $(".vendor-info .name").each(function() {
+    var $resto = $(this);
+    var $envoi = $resto.next();
+    var restaurants_name = encodeURI($resto.text().replace(/^\s+|\s+$/g, ''));
+    $.ajax({
+      type: "GET",
+      url: "https://localhost:3000/api/v1/restaurants/" + restaurants_name,
+      success: function(data) {
+        if (data.ranking != undefined) {
+          $envoi.append($('<span>').text("・ note:" + " " + data.ranking));
+        } else {
+          $envoi.append($('<span>').text("・" + "" + " N/A"));
+        }
+      },
+      error: function(jqXHR) {
+        console.log('error')
+      }
+    });
+  });
+}
 
+function enhanceDeliverooUI() {
   $(".restaurant-index-page-tile").each(function() {
     var $resto = $(this);
     var restaurants_name = encodeURI($resto.find('h3').text());
@@ -18,26 +39,17 @@ $(function() {
       }
     });
   });
+}
 
-  $(".vendor-info .name").each(function() {
-    var $resto = $(this);
-    var $envoi = $resto.next()
-    var restaurants_name = encodeURI($resto.text());
-    $.ajax({
-      type: "GET",
-      url: "https://localhost:3000/api/v1/restaurants/" + restaurants_name,
-      success: function(data) {
-        if (data.ranking != undefined) {
-        $envoi.append($('<span>').text("・ note:" + " " + data.ranking)).css();
-        } else {
-        $envoi.append($('<span>').text("・" + "" + " N/A"));
-        }
-      },
-      error: function(jqXHR) {
-        console.log('error')
-      }
-    });
-  });
+function isFoodora() {
+  return /foodora/.test(window.location.href);
+}
 
+$(function() {
+  if (isFoodora()) {
+    enhanceFoodoraUI();
+    $(document).on('page:load', function() { enhanceFoodoraUI(); });
+  } else {
+    enhanceDeliverooUI();
+  }
 });
-
