1d194ab83bb5f2234e745234297137a2f1f8a5db
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-06-08 17:11:18 +0200

    improve search

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   include PgSearch
 
-  pg_search_scope :search, against: [:name, :address], using: :tsearch
+  pg_search_scope :search, against: [:name, :address], using: { tsearch: { any_word: true } }
 end
