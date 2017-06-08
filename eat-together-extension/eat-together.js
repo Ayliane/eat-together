29fcbb7773c2e63abf7c08242fd9fd5170e3cec0
Guillaume Grand <guillaume.grand80@orange.fr>
2017-06-08 18:28:26 +0200

    Update star-color to contrast with bg

--- eat-together-extension/eat-together.js
+++ eat-together-extension/eat-together.js
@@ -61,7 +61,7 @@ function isFoodora() {
 }
 
 $(function() {
-  $('head').append("<style>.stars-wrapper{background:rgba(255,255,255,0.8);position:absolute;bottom:0;left:0;z-index:5;}.star-icon{padding:0 1px;position:relative;color:#ddd;font-size:1.5em;}.star-icon.full:before{text-shadow:0 0 2px rgba(0,0,0,0.7);color:#FDE16D;content:'\\2605';position:absolute;left:0}.star-icon.half:before{text-shadow:0 0 2px rgba(0,0,0,0.7);color:#FDE16D;content:'\\2605';position:absolute;left:0;width:50%;overflow:hidden}@-moz-document url-prefix(){.star-icon { font-size:50px;line-height:34px}</style>");
+  $('head').append("<style>.stars-wrapper{background:rgba(255,255,255,0.8);position:absolute;bottom:0;left:0;z-index:5;}.star-icon{padding:0 1px;position:relative;color:#aaa;font-size:1.5em;}.star-icon.full:before{text-shadow:0 0 2px rgba(0,0,0,0.7);color:#FDE16D;content:'\\2605';position:absolute;left:0}.star-icon.half:before{text-shadow:0 0 2px rgba(0,0,0,0.7);color:#FDE16D;content:'\\2605';position:absolute;left:0;width:50%;overflow:hidden}@-moz-document url-prefix(){.star-icon { font-size:50px;line-height:34px}</style>");
   if (isFoodora()) {
     enhanceFoodoraUI();
     $(document).on('page:load', function() { enhanceFoodoraUI(); });
